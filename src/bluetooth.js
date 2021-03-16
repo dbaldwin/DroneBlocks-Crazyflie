bab2033afbe10a1d3f6cd4d38d0287783b410739
Dennis Baldwin <dennisbaldwin@gmail.com>
2021-03-16 14:06:00 -0500

    first commit

---
+++ src/bluetooth.js
@@ -0,0 +1,12 @@
+export default class Bluetooth {
+
+    // const serviceUUID     = '00000201-1c7f-4f9e-947b-43b7c00a9a08';
+    // const exCharUUID      = '00000202-1c7f-4f9e-947b-43b7c00a9a08';
+    // const txCharUUID      = '00000203-1c7f-4f9e-947b-43b7c00a9a08';
+    // const rxCharUUID      = '00000204-1c7f-4f9e-947b-43b7c00a9a08';
+
+    constructor() {
+        console.log("Bluetooth constructor");
+    }
+
+}
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import nipplejs from 'nipplejs';
+import Bluetooth from './bluetooth';
+
+window.init = function() {
+
+    new Bluetooth();
+
+    const left = nipplejs.create({
+        zone: document.getElementById('left_stick'),
+        mode: 'static',
+        position: { left: '50%', top: '50%' },
+        color: 'red',
+        size: 200
+    });
+
+    const right = nipplejs.create({
+        zone: document.getElementById('right_stick'),
+        mode: 'static',
+        position: { left: '50%', top: '50%' },
+        color: 'blue',
+        size: 200
+    });
+
+    let leftdx = 0;
+    let leftdy = 0;
+    const size = left.options.size;
+
+    left.on('move', function (evt, nipple) {
+        leftdx = nipple.position.x;
+        leftdy = nipple.position.y;
+        
+        var throttle = ((nipple.distance * Math.sin(nipple.angle.radian)) / size);
+        var yaw = ((nipple.distance * Math.cos(nipple.angle.radian)) / size);
+
+        //console.log(nipple.distance);
+        //console.log(nipple.distance);
+        
+        console.log("throttle: " + throttle + ", yaw: " + yaw);
+
+    });
+
+}
